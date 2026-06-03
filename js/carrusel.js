7bea6afd6c01618b0c35243c541f2fa83d97dc48
MiguelVergara160 <20243ds069@utez.edu.mx>
2026-06-02 19:05:51 -0600

    tercer commit

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,20 +1,23 @@
 var imagenes = [
-    "images (1).jpg",
-    "images (2).jpg",
-    "images (3).jpg",
-    "images (4).jpg",
-    "images (5).jpg",
+    "img/images (1).jpg",
+    "img/images (2).jpg",
+    "img/images (3).jpg",
+    "img/images (4).jpg",
+    "img/images (5).jpg"
 ];
 
 var i = 0;
 
-document.getElementById("siguiente").addEventListener("click", () =>{
-    if(i<4){i++;}
-    document.getElementById("carrusel").setAttribute("src", imagenes[i]);
+document.getElementById("siguiente").addEventListener("click", () => {
+    if (i < 4) {
+        i++;
+    }
+    document.getElementById("carrusel").src = imagenes[i];
 });
 
-document.getElementById("anterior").addEventListener("click", () =>{
-    if(i<0){i--;}
-    document.getElementById("carrusel").setAttribute("src", imagenes[i]);
+document.getElementById("anterior").addEventListener("click", () => {
+    if (i > 0) {
+        i--;
+    }
+    document.getElementById("carrusel").src = imagenes[i];
 });
-
